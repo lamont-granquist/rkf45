cbed902fb49b6b587934c7f1abb6f2a03e7520ea
mollerhoj <mollerhoj3@gmail.com>
2013-03-06 18:52:34 +0100

    2,3 is implemented fake now

--- c/Policy_Distributor.c
+++ c/Policy_Distributor.c
@@ -4,7 +4,7 @@
 #include "PureEndowment.h"
 #include "Policy_Distributor.h"
 #include "DeferredTemporaryLifeAnnuity.h"
-//#include "TemporaryLifeAnnuityPremium.h"
+#include "TemporaryLifeAnnuityPremium.h"
 
 int policy = 0;
 
@@ -18,7 +18,7 @@ void dy(double t, double* V, double* result) {
       dy_DeferredTemporaryLifeAnnuity(t,V,result);
     break;
     case 3:
-      //dy_TemporaryLifeAnnuityPremium(t,V,result);
+      dy_TemporaryLifeAnnuityPremium(t,V,result);
     break;
     case 4:
       dy_TermInsurance(t,V,result);
@@ -36,7 +36,7 @@ void bj_ii(double t, double* result) {
       bj_ii_DeferredTemporaryLifeAnnuity(t,result);
     break;
     case 3:
-      //bj_ii_TemporaryLifeAnnuityPremium(t,result);
+      bj_ii_TemporaryLifeAnnuityPremium(t,result);
     break;
     case 4:
       bj_ii_TermInsurance(t,result);
@@ -54,7 +54,7 @@ double** test_values() {
       return tv_DeferredTemporaryLifeAnnuity();
     break;
     case 3:
-      //return tv_TemporaryLifeAnnuityPremium();
+      return tv_TemporaryLifeAnnuityPremium();
     break;
     case 4:
       return tv_TermInsurance();
