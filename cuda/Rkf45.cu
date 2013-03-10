be4994e1ec4fe4f14474aac5691865a78cd0a18d
mollerhoj <mollerhoj3@gmail.com>
2013-03-10 10:42:24 +0100

    trying to get global calls to work

--- cuda/Rkf45.cu
+++ cuda/Rkf45.cu
@@ -1,11 +1,340 @@
+/*
+ * C implementation of the Rkf45 algoritm.
+ */
+
+
+/********************* INIT *******************/
+/*
+//Library inclusion
+#include <stdlib.h>
 #include <stdio.h>
+#include "Policy_Distributor.h" 
+#include <assert.h>
+#include <math.h>
+#include "Matrix_Library.h"
+#include <unistd.h> //only for sleep?
+#include <string.h> //only for sleep?
+#include "Boolean.h"
+
+//Max,min,sign functions
+#define max(a,b) ({ __typeof__ (a) _a = (a); __typeof__ (b) _b = (b);_a > _b ? _a : _b; })
+#define min(a,b) ({ __typeof__ (a) _a = (a); __typeof__ (b) _b = (b);_a < _b ? _a : _b; })
+#define sign(x)  ((x > 0) - ( x < 0))
+
+//Declare functions
+bool is_equal();
+static void test_all();
+static void time_all();
+static double time_one();
+static bool local_start_to_be_reached();
+static void allocate_equation_space();
+static void xpy();
+static void calculate_solutions();
+static void local_estimate();
+static double calculate_initial_stepsize();
+static double scale_from_error();
+static double FindDoubleEpsilon();
+
+//Declare Estimator variables
+
+//Public variables
+int neqn;
+int start_year;
+int end_year;
+// dy       //policy
+// bj_ii
+double relerr;
+double abserr;
+double* end_year_y; 
+
+//Private variables
+static double t;
+static double stepsize;
+static double* f1;
+static double* f2;
+static double* f3;
+static double* f4;
+static double* f5;
+static double* f_swap;
+static double* y;
+static double* y_diff;
+static double* y_plus_one;
+static double* y_plus_one_alternative;
+static int local_start_year;
+static int local_end_year;
+static double DoubleEpsilon;
+
+static int m; //Result length;
+*/
+/******************* Constructor *********************/
+
+/* Construct */
+/*void construct(int n) {
+  neqn = n;
+  DoubleEpsilon = FindDoubleEpsilon();
+  allocate_equation_space();
+}*/
+
+/* Allocate equation space */
+/*static void allocate_equation_space() {
+  //Global for the class
+  y_plus_one             = malloc(sizeof(double)*neqn);
+  end_year_y             = malloc(sizeof(double)*neqn);
+  y                      = malloc(sizeof(double)*neqn);
+  y_diff                 = malloc(sizeof(double)*neqn);
+
+  //Temporary for the solve method
+  f1                     = malloc(sizeof(double)*neqn);
+  f2                     = malloc(sizeof(double)*neqn);
+  f3                     = malloc(sizeof(double)*neqn);
+  f4                     = malloc(sizeof(double)*neqn);
+  f5                     = malloc(sizeof(double)*neqn);
+  f_swap                 = malloc(sizeof(double)*neqn);
+  y_plus_one_alternative = malloc(sizeof(double)*neqn);
+}
+*/
+/********************** Solve *********************/
+
+/* Calculate the actual and the alternative solutions */
+//y_plus_one and y_plus_one_alternative will be set
+/*static void calculate_solutions() {
+
+  double lcd_stepsize = stepsize / 4.0; //lowest common denominator of stepsize
+
+  //f1
+  for (int i = 0; i < neqn; i++ )
+    f_swap[i] = y[i] + lcd_stepsize * y_diff[i];
+  dy ( t + lcd_stepsize, f_swap, f1 );
+
+  //f2
+  lcd_stepsize = 3.0 * stepsize / 32.0;
+  for (int i = 0; i < neqn; i++ )
+    f_swap[i] = y[i] + lcd_stepsize * ( y_diff[i] + 3.0 * f1[i] );
+  dy ( t + 3.0 * stepsize / 8.0, f_swap, f2 );
+
+  //f3
+  lcd_stepsize = stepsize / 2197.0;
+  for (int i = 0; i < neqn; i++ )
+    f_swap[i] = y[i] + lcd_stepsize * ( 1932.0 * y_diff[i] + ( 7296.0 * f2[i] - 7200.0 * f1[i] ) );
+  dy ( t + 12.0 * stepsize / 13.0, f_swap, f3 );
+
+  //f4
+  lcd_stepsize = stepsize / 4104.0;
+  for (int i = 0; i < neqn; i++ )
+    f_swap[i] = y[i] + lcd_stepsize * ( ( 8341.0 * y_diff[i] - 845.0 * f3[i] ) + 
+        ( 29440.0 * f2[i] - 32832.0 * f1[i] ) );
+  dy ( t + stepsize, f_swap, f4 );
+
+  //f5
+  lcd_stepsize = stepsize / 20520.0;
+  for (int i = 0; i < neqn; i++ )
+    f_swap[i] = y[i] + lcd_stepsize * ( ( -6080.0 * y_diff[i] + 
+          ( 9295.0 * f3[i] - 5643.0 * f4[i] ) ) + ( 41040.0 * f1[i] - 28352.0 * f2[i] ) );
+  dy ( t + stepsize / 2.0, f_swap, f5 );
+
+  //Calculate solution
+  lcd_stepsize = stepsize / 7618050.0;
+  for (int i = 0; i < neqn; i++ )
+    y_plus_one[i] = y[i] + lcd_stepsize * ( ( 902880.0 * y_diff[i] + 
+          ( 3855735.0 * f3[i] - 1371249.0 * f4[i] ) ) + ( 3953664.0 * f2[i] + 277020.0 * f5[i] ) );
+
+  //Calculate alternative solution
+  for (int i = 0; i < neqn; i++ )
+    y_plus_one_alternative[i] = ( -2090.0 * y_diff[i] + ( 21970.0 * f3[i] - 15048.0 * f4[i] ) ) + ( 22528.0 * f2[i] - 27360.0 * f5[i] );
+
+}
+*/
+/* Calculate the error of the solution */
+//Pure
+/*static double calculate_solution_error() {
+
+  //Used in calculations
+  double scale = 2.0 / relerr;
+
+  //Calculate the biggest_difference
+  double biggest_difference = 0.0;
+  for (int i = 0; i < neqn; i++ )
+  {
+    double et = fabs( y[i] ) + fabs( y_plus_one[i] ) + scale * abserr;
+    double ee = fabs( y_plus_one_alternative[i] );
+
+    biggest_difference = max ( biggest_difference, ee / et );
+  }
+  
+  //Return the error
+  return fabs( stepsize ) * biggest_difference * scale / 752400.0;
+}
+*/
+/******************* Local estimation ***********************/
+
+/* Move from current position to local_start_year, and update all values */
+// Updates y, h
+/*
+static void local_estimate() {
+  
+  //Step by step integration.
+  bool local_start_reached = false;
+  while (!local_start_reached)
+  {
+    //Variables used in calculations
+    bool stepsize_descresed = false;
+    double hmin = 26.0 * DoubleEpsilon * fabs( t );
+
+    local_start_reached = local_start_to_be_reached();
+
+    calculate_solutions();
+    double error = calculate_solution_error();
+
+    //Integreate 1 step
+    while(error > 1.0)
+    {
+      stepsize_descresed = true;
+      local_start_reached = false;
+
+      //Scale down.
+      double s = max(0.1,0.9 / pow( error, 0.2 ));
+      stepsize = s * stepsize;  
+
+      //Try again.
+      calculate_solutions();
+      error = calculate_solution_error();
+    }
+
+
+    //Advance in time
+    t = t + stepsize; 
+
+    //Apply solution
+    for (int i = 0; i < neqn; i++ )
+      y[i] = y_plus_one[i];
+
+
+    //Update y_diff
+    dy ( t, y, y_diff );
+
+    //Apply scale to stepsize
+    double scale = scale_from_error(error,stepsize_descresed);
+    stepsize = sign ( stepsize ) * max ( scale * fabs( stepsize ), hmin );
+  }
+}*/
+
+/**************** Local estimation help functions ****************/
+
+
+/* React if the "local start year" is about to be reached */
+//Effects stepsize, returns whether the start year is reached
+/*static bool local_start_to_be_reached() {
+    double dt = local_end_year - t;
+    if ( 2.0 * fabs( stepsize ) > fabs( dt ) )
+    {
+      if ( fabs( dt ) <= fabs( stepsize ) ) //Final step?
+      {
+        stepsize = dt;                   //Let stepsize hit output point
+        return true;
+      }
+      else
+      {
+        stepsize = 0.5 * dt; // If not final step, set stepsize to be second final step. (evens out)
+      }
+    }
+    return false;
+}*/
+
+/* Calculate stepsize's startvalue */
+/*
+static double calculate_initial_stepsize()
+{
+  //Calculate the start value of stepsize
+  double stepsize = fabs( start_year - t );
+
+
+  for (int k = 0; k < neqn; k++ )
+  {
+    double tol = relerr * fabs( y[k] ) + abserr;
+    if ( 0.0 < tol )
+    {
+      double ypk = fabs( y_diff[k] );
+      if ( tol < ypk * pow( stepsize, 5 ) )
+      {
+        stepsize = pow( ( tol / ypk ), 0.2 );
+        printf("this should not happen.\n");
+      }
+    }
+  }
+
+  return  max( stepsize, 26.0 * DoubleEpsilon * max( fabs( t ), fabs( start_year - t ) ) );
+}
+*/
+
+/* Scale from error calculations */
+/*
+static double scale_from_error(double error,bool stepsize_decreased) {
+  double scale = min(5.0,0.9 / pow( error, 0.2 ));
+
+  if (stepsize_decreased)
+    scale = min( scale, 1.0 );
+
+  return scale;
+}
+*/
+
+/*********************** Estimate **************************/
+
+/* Estimate range */
+__device__ double estimate() {
+  /*
+
+  for(int i = 0;i<neqn;i++)                // y
+    y[i] = end_year_y[i];
+  t = (double) end_year;                   // t
+  dy( t, y, y_diff);                       // y_diff
+  stepsize = calculate_initial_stepsize(); // stepsize
+
+  //Allocate result matrix, calculate m (length of result)
+  m = end_year-start_year+1;
+  double** result = allocate_double_matrix(m,neqn);
+
+  //Solve for one year at a time
+  for (int year=end_year; year>start_year; year--) {
+
+    //Add this years benefit to y
+    bj_ii(year,y);
+
+    // Integrate over [year,year-1]
+    local_start_year = year;
+    local_end_year = year-1;
+    local_estimate();
+
+    //Copy y to results
+    for(int i=0;i<neqn;i++)
+      result[year-start_year-1][i] = y[i];
+  }
+
+  return result;
+  */
+
+  return 2.0;
+}
+
+/*************************** Auxiliaries ****************************/
+
+/* Find double epsilon */
+/*static double FindDoubleEpsilon() {
+  double r = 1.0;
+  while (1.0 < (1.0 + r))
+    r = r / 2.0;
+  return 2.0 * r;
+}*/
+
+/**************************** DEVICE ******************************/
 
 typedef struct
 {
-    int a, b;
-} point;
-
-/**************************** DEVICE ******************************/
+  int policy;
+  int end_year;
+  double abserr;
+  double relerr;
+} CUSTOMERS;
 
 //Calculate the id
 __device__ int get_id(void) {
@@ -26,73 +355,13 @@ __device__ int get_n_device(void) {
 }
 
 // Device code
-__global__ void kernel(point *myPoint, int *result) {
+__global__ void kernel(CUSTOMERS *customers, int *result) {
   int id = get_id();
 
   //construct(2);
+  //estimate();
 
   // Use the thread ID as an index
-  result[id] = myPoint[id].a; //neqn; //get_n();//idz; //dev_a[tid] + dev_b[tid];
+  result[id] = customers[id].policy; //neqn; //get_n();//idz; //dev_a[tid] + dev_b[tid];
 }
 
-/**************************** HOST ******************************/
-
-//Calculate the number of kernels
-int get_n_host(dim3 block_dim,dim3 grid_dim) {
-  return grid_dim.x * grid_dim.y * grid_dim.z * block_dim.x * block_dim.y * block_dim.z; 
-}
-
-// Host code
-int main(int argc, char const *argv[]) {
-
-  //HOST:
-  //construct (decide neqn)
-
-  //Set public vars.
-  //(Must be viewable from device)
-
-  //Start kernel
-  //(estimate);
-
-  dim3 block_dim(2,2,3); //Number of threads per block
-  dim3 grid_dim(2,3,1);  //Number of blocks per grid (cc. 1.2 only supports 2d)
-  int nsize = get_n_host(block_dim,grid_dim); 
-
-  // Data on the host and the device, respectively
-  int result[nsize];
-  int *dev_result;
-  point *dev_myPoint; 
-  point *myPoint;
-
-  myPoint = (point*) malloc(sizeof(point)*nsize);
-  myPoint[0].a = 3;
-  myPoint[0].b = 4;
-  myPoint[1].a = 7;
-  myPoint[1].b = 19;
-  myPoint[30].a = 17;
-
-  // Allocate memory on the device
-  cudaMalloc((void**)&dev_myPoint, sizeof(point) * nsize);
-  cudaMalloc((void**)&dev_result, sizeof(int) * nsize);
-
-  // Copy data to the device
-  cudaMemcpy(dev_myPoint, myPoint, sizeof(point) * nsize, cudaMemcpyHostToDevice);
-
-  // Launch the kernel with 10 blocks, each with 1 thread
-  kernel <<<grid_dim, block_dim>>>(dev_myPoint,dev_result);
-
-  // Copy the result back from the device
-  cudaMemcpy(result, dev_result, sizeof(int) * nsize, cudaMemcpyDeviceToHost);
-
-  // Print the result: "0 5 20 45 80 125 180 245 320 405"
-  for(int i = 0; i < nsize; i++) {
-    printf("%d ", result[i]);
-  }
-
-  printf("\n");
-
-  cudaFree(dev_customer);
-  cudaFree(dev_result);
-
-  return 0;
-}
