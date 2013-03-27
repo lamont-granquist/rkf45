798cc58e9d951819332b053d0a6e400072069e11
mollerhoj <mollerhoj3@gmail.com>
2013-03-27 10:57:09 +0100

    starting to work on sorting

--- cuda/Launcher.cu
+++ cuda/Launcher.cu
@@ -14,10 +14,10 @@ int get_n_host(dim3 block_dim,dim3 grid_dim) {
 int main(int argc, char const *argv[]) {
 
   /********** 0. SETUP **********/
-  dim3 block_dim(8,8,5); //Number of threads per block // 320 seems to be best
-  dim3 grid_dim(32,16,1);  //Number of blocks per grid (cc. 1.2 only supports 2d)
-  //dim3 block_dim(2,2,1); //Number of threads per block
-  //dim3 grid_dim(2,1,1);  //Number of blocks per grid (cc. 1.2 only supports 2d)
+  //dim3 block_dim(8,8,5); //Number of threads per block // 320 seems to be best
+  //dim3 grid_dim(32,16,1);  //Number of blocks per grid (cc. 1.2 only supports 2d)
+  dim3 block_dim(2,2,1); //Number of threads per block
+  dim3 grid_dim(2,1,1);  //Number of blocks per grid (cc. 1.2 only supports 2d)
 
   int nsize = get_n_host(block_dim,grid_dim); 
 
