cde6d2b4269a9a25001f71ac66fb72ddb42ff2e1
mollerhoj <mollerhoj3@gmail.com>
2013-04-01 18:09:34 +0200

    slow math

--- cuda/Rkf45.cu
+++ cuda/Rkf45.cu
@@ -161,7 +161,7 @@ static void local_estimate(int policy,int age, int neqn,int local_end_year,int l
       local_start_reached = false;
 
       //Scale down.
-      float s = max(0.1f,0.9f / __powf( error, 0.2f ));
+      float s = max(0.1f,0.9f / powf( error, 0.2f ));
       *stepsize = s * *stepsize;  
 
       //Try again.
@@ -222,9 +222,9 @@ static float calculate_initial_stepsize(int neqn,int start_year,int end_year,flo
     if ( 0.0f < tol )
     {
       float ypk = fabsf( y_diff[k] );
-      if ( tol < ypk * __powf( s, 5.0f ) )
+      if ( tol < ypk * powf( s, 5.0f ) )
       {
-        s = __powf( ( tol / ypk ), 0.2f );
+        s = powf( ( tol / ypk ), 0.2f );
         //printf("this should not happen.\n");
       }
     }
@@ -236,7 +236,7 @@ static float calculate_initial_stepsize(int neqn,int start_year,int end_year,flo
 /* Scale from error calculations */
 __device__ 
 static float scale_from_error(float error,bool stepsize_decreased) {
-  float scale = min(5.0f,0.9f / __powf( error, 0.2f ));
+  float scale = min(5.0f,0.9f / powf( error, 0.2f ));
 
   if (stepsize_decreased)
     scale = min( scale, 1.0f );
@@ -372,7 +372,7 @@ void cpu_kernel(CUSTOMERS customers,float *result) {
 
 __device__ 
 float GM(int age, float t) {
-    return 0.0005f + __powf(10.0f, 5.728f - 10.0f + 0.038f*((float)age + t));
+    return 0.0005f + powf(10.0f, 5.728f - 10.0f + 0.038f*((float)age + t));
 }
 
 // Interest
@@ -540,7 +540,7 @@ static float b_1_DisabilityAnnuity(float t) {
 
 __device__ 
 static float GM01_DisabilityAnnuity(int age, float t) {
-  return 0.0006f + __powf(10.0f, 4.71609f - 10.0f + 0.06f*((float)age + t));
+  return 0.0006f + powf(10.0f, 4.71609f - 10.0f + 0.06f*((float)age + t));
 }
 
 __device__ 
@@ -622,7 +622,7 @@ static float b_1_DisabilityTermInsurance(float t) {
 
 __device__ 
 static float GM01_DisabilityTermInsurance(int age, float t) {
-  return 0.0006f + __powf(10.0f, 4.71609f - 10.0f + 0.06f*((float)age + t));
+  return 0.0006f + powf(10.0f, 4.71609f - 10.0f + 0.06f*((float)age + t));
 }
 
 __device__ 
